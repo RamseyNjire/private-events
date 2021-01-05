adf9d9f3d4d078c35023a278eb9c178cef46f476
Ramsey Njire <ramseynjire@gmail.com>
2021-01-05 22:55:29 +0300

    create events controller, views, and REST routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
   devise_for :users
   resources :users, only: [:show]
+  resources :events, only: [:index, :new, :create, :show]
 end
