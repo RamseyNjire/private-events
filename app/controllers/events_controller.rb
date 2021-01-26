482d73c2f4487d988c463f76890f2085e2de2cc7
Ramsey Njire <ramseynjire@gmail.com>
2021-01-26 23:57:43 +0300

    Add new gems for testing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,16 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = current_user.events.build
   end
 
   def create
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :user_signed_in?, only: [:show]
   def show
     @user = User.find(params[:id])
   end
