1fd6dfa15c1ceee78c03a0ecd9448786ecf46001
Ramsey Njire <ramseynjire@gmail.com>
2021-01-27 00:05:41 +0300

    rollback DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :events, foreign_key: "creator_id", dependent: :destroy
+  has_many :events, foreign_key: :creator_id
+  has_many :invitations, foreign_key: :attendee_id
+  has_many :attended_events, through: :invitations, source: :attended_event
+  validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 11 }
 end
