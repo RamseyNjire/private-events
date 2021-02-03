728f0d3f50064fb29a7667a022d4bfba07cfc3e2
Ramsey Njire <ramseynjire@gmail.com>
2021-02-04 00:16:57 +0300

    correct Rubocop offenses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   include SessionsHelper
 
-  def index 
-  end
+  def index; end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,14 +4,14 @@ require 'capybara/rspec'
 
 RSpec.describe Event, type: :model do
   context 'validation tests' do
-    it "Should only save if there is a date" do
+    it 'Should only save if there is a date' do
       e = Event.new(creator_id: 1, description: 'Event 1')
       expect(e.save).to eq(false)
     end
 
-    it "Should note save descriptions with more than 300 characters" do
+    it 'Should note save descriptions with more than 300 characters' do
       bigstr = ''
-      301.times { |i| bigstr + 'str '}
+      301.times { |_i| "#{bigstr}str " }
       e = Event.new(creator_id: 1, date: '2020-05-20', description: bigstr)
       expect(e.save).to eq(false)
     end
