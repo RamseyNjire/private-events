3bfbf09b11a68bbf9ec89d006eba6b32fc51b29e
Ramsey Njire <ramseynjire@gmail.com>
2021-02-03 22:08:59 +0300

    remove commented code in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,13 +4,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invitations, source: :attendee
   scope :past, -> { where('date < ?', DateTime.now) }
   scope :upcoming, -> { where('date >= ?', DateTime.now) }
-  # def self.created_before(time)
-  #   where('date <= ?', time)
-  # end
-
-  # def self.upcoming_events(time)
-  #   where('date > ?', time)
-  # end
   validates :date, presence: true
   validates :description, presence: true, length: { maximum: 300 }
 end
